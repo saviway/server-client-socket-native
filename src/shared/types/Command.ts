65f74f4a9cea52b3f88420341a0166121690ebe7
Ivan Savinov <nofl3x@protonmail.com>
2023-03-15 19:15:50 +0300

    feat: add Command type

---
+++ src/shared/types/Command.ts
@@ -0,0 +1,5 @@
+export type Command = 'Subscribe' | 'Unsubscribe' | 'CountSubscribers'
+
+export const CMD_SUBSCRIBE: Command = 'Subscribe'
+export const CMD_UNSUBSCRIBE: Command = 'Unsubscribe'
+export const CMD_COUNT_SUBSCRIBERS: Command = 'CountSubscribers'
---
+++ src/shared/types/index.ts
@@ -0,0 +1,2 @@
+export type { Command } from './Command'
+export { CMD_SUBSCRIBE, CMD_UNSUBSCRIBE, CMD_COUNT_SUBSCRIBERS } from './Command'
